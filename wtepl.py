ff80fe2d5e1ae55bf5fc3703ce9b32be09dd95b6
WTAnalysis <wt_analysis@outlook.com>
2025-08-15 14:06:06 +0100

    Update wtepl.py

--- wtepl.py
+++ wtepl.py
@@ -52,7 +52,8 @@ headers = {
     'Referer': 'https://www.scoresway.com/',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
 }
-
+homeimage = None
+awayimage = None
 schedule_df = pd.DataFrame()
 if dataafterleague:
     all_matches, page, page_size = [], 1, 400
